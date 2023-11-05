8bace67f5a8551de9b1ccd226b7d62edecef669d
margaretrl <magylutzkanov@gmail.com>
2023-11-05 00:37:40 -0400

    cleanup

--- part3/src/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/cop4610/project-2-group-8/part3/src/modules.order := {   echo /home/cop4610/project-2-group-8/part3/src/elevator.o; :; } > /home/cop4610/project-2-group-8/part3/src/modules.order
